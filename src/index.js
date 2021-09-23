b47cd9482bc80181f13a5017623d58767fd7166b
ViacheslavSosnovskiy <saintstill333@gmail.com>
2021-09-23 06:37:55 +0300

    +

--- src/index.js
+++ src/index.js
@@ -1,25 +1,32 @@
-import React from "react";
 import ReactDom from "react-dom";
+// import user from "./user.json";
 
-const element = React.createElement("div", { a: 5 });
-console.log(element);
+function Profile(props) {
+  console.log(props);
+  //   return (
+  // <div class="profile">
+  //   <div class="description">
+  //     <img src={user.avatar} alt="Аватар пользователя" class="avatar" />
+  //     <p class="name">{user.name}</p>
+  //     <p class="tag">{user.tag}</p>
+  //     <p class="location">{user.location}</p>
+  //   </div>
 
-ReactDom.render();
-// ==========================
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+  //   <ul class="stats">
+  //     <li>
+  //       <span class="label">Followers</span>
+  //       <span class="quantity">{user.stats.followers}</span>
+  //     </li>
+  //     <li>
+  //       <span class="label">Views</span>
+  //       <span class="quantity">{user.stats.views}</span>
+  //     </li>
+  //     <li>
+  //       <span class="label">Likes</span>
+  //       <span class="quantity">{user.stats.likes}</span>
+  //     </li>
+  //   </ul>
+  // </div>
+  //   );
+}
+ReactDom.render(<Profile a="4" />, document.querySelector("#root"));
