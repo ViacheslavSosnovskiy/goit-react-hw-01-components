0543312ee9f9b0098ceb4532aa1d942b97515ed5
ViacheslavSosnovskiy <saintstill333@gmail.com>
2021-09-26 05:20:49 +0300

    task4 done

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import Profile from "./Profile/Profile";
 import Statistics from "./Statistics/Statistics";
 import FriendList from "./FriendList/FriendList";
-// --------------------------------------------
+import TransactionHistory from "./TransactionHistory/TransactionHistory";
+// ----------------------------------------------------------------------
 import user from "../data/user.json";
 import statisticalData from "../data/statistical-data.json";
 import friendsData from "../data/friends.json";
+import transactions from "../data/transactions.json";
 
 const App = () => {
   return (
@@ -19,6 +21,7 @@ const App = () => {
       />
       <Statistics title="Upload stats" stats={statisticalData} />
       <FriendList friends={friendsData} />
+      <TransactionHistory items={transactions} />;
     </div>
   );
 };
